38493dc07d18894e18e3c958503b63e16d5f25e2
jovanahurra82 <josy82@gmail.com>
2024-11-12 18:05:08 +0100

    updatre pass

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -2,10 +2,14 @@ import { expect, test } from '@playwright/test';
 import { LoginPage } from '../pages/loginpage';
 import { StorePage } from '../pages/storepage';
 
-
+let password: string
 test('Login with Markus', async ({ page }) => {    
     const loginPage = new LoginPage(page);
     const storePage = new StorePage(page);
+    if (process.env.PASSWORD !== undefined){
+        password: string = process.env.PASSWORD
+    }
+     
     await page.goto("http://hoff.is/login")
     await loginPage.login("Markus", "sup3rs3cr3t", 'consumer');
     const header = await storePage.header.textContent()  
