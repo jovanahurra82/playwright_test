821c9f5f2d65bed8d09ba95357321dde70963403
jovanahurra82 <josy82@gmail.com>
2024-11-12 17:15:26 +0100

    undo pass

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  console.log("The value is: " + process.env.PASSWORD);
+  await page.goto('https://playwright.dev/');  
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
