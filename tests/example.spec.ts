7260650a8ebd5c730f7d328bba1dc8c3f69951b3
jovanahurra82 <josy82@gmail.com>
2024-11-12 17:10:39 +0100

    password

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
+  console.log("The value is: " + process.env.PASSWORD);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
